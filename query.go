614c0806d41744a00800c6a60feb16a8845805fc
Mike <mjmorell@liberty.edu>
2018-06-25 10:00:39 -0400

    Cleaned up extra data in query.go

--- query.go
+++ query.go
@@ -91,18 +91,3 @@ func ISLIKE(str string) string {
 func ISNOTLIKE(str string) string {
 	return "NOT%20LIKE" + str
 }
-
-/*
-func tester() {
-	query := Filter("cat_item") + IS("cd13f3032b7892001235717bf8da1580") +
-		AND() + Filter("ref_u_computer_support.u_depot_location") + IS("862a49f29c806d40f47c19537d850ca4") +
-		AND() + Filter("state") + ISONEOF("1", "2", "11") +
-		OR() + Filter("u_substate") + IS("c4b3f3702b3c9200258f89efe8da1504")
-
-	or := "cat_item=cd13f3032b7892001235717bf8da1580" +
-		"^ref_u_computer_support.u_depot_location=862a49f29c806d40f47c19537d850ca4" +
-		"^stateIN1,2,11" +
-		"^ORu_substate=c4b3f3702b3c9200258f89efe8da1504"
-
-	fmt.Printf(query + or)
-} */
